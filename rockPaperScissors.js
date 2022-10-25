c4b0c68be06ea9cd860049ec9021de59aceb5bcb
Art <mnsenna@protonmail.com>
2022-10-25 19:40:44 +1000

    Implement playRound function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,10 +2,10 @@ console.log("hello world")
 
 //write function to return random string
 
-function getComputerChoice() {
+function getRandomChoice() {
     const random = Math.floor(Math.random() * 3);
     let choice = '';
-    console.log(random);
+    // console.log(random);
     if (random === 0) {
         choice = "Scissors"
     } else if (random === 1) {
@@ -13,8 +13,23 @@ function getComputerChoice() {
     } else {
         choice = "Paper"
     }
-    console.log(choice);
-    return choice;
+    // console.log(choice);
+    return choice
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log(`Tie ${playerSelection} does not beat ${computerSelection}`)
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return `Computer loses ${playerSelection} beats ${computerSelection}`
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return `Computer loses ${playerSelection} beats ${computerSelection}`
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return `Computer loses ${playerSelection} beats ${computerSelection}`
+    } else {
+        return `Player loses ${playerSelection} does NOT beat ${computerSelection}`
+    }
+}
+
+console.log(playRound(getRandomChoice(), getRandomChoice()));
+
